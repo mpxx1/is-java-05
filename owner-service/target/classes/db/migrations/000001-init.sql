eceaa88d5261eccc8f8e7b4eca5c31a610e9c994
Makar A. Pestrikov <mpxx088@gmail.com>
2024-06-15 19:19:33 +0300

    target owner fix

--- owner-service/target/classes/db/migrations/000001-init.sql
+++
@@ -1,7 +0,0 @@
-
-create table owners (
-    id          bigint          not null    primary key,
-    email       varchar(100)    not null    unique,
-    name        varchar(50),
-    birthday    date
-);
