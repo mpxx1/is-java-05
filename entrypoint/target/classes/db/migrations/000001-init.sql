2102631b9a41b964229144ff6ce5c2d2e8c735f1
Makar A. Pestrikov <mpxx088@gmail.com>
2024-06-15 19:11:39 +0300

    target fix

--- entrypoint/target/classes/db/migrations/000001-init.sql
+++
@@ -1,9 +0,0 @@
-
-
-create table users (
-    id          bigint                          primary key,
-    username    varchar(100)    not null        unique,
-    pass_hash    varchar(200)   not null,
-    salt        varchar(50)     not null,
-    role        varchar(10)     not null        check ( role in ( 'ADMIN', 'BASE' ) )
-);
